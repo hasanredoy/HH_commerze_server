68ef43cff4c80483f6f3e767dc50913509caa8ca
unknown <redoyhasan4585@gmail.com>
2024-07-16 14:41:40 +0400

    added route for login user

--- index.js
+++ index.js
@@ -39,23 +39,65 @@ async function run() {
      const result = await usersCollection.find().toArray()
      res.send(result)
     })
+    // get users for login  
+    app.post('/user_login', async(req,res)=>{
+      // get user data 
+      const userDataForLogin = req.body
+      const userEmail= userDataForLogin.email
+      const userPhone= userDataForLogin.phone
+      const userPin= userDataForLogin.pin
+
+      // check PIN 
+      
+      const filter = {$or:[{email:userEmail},{phone:userPhone}]}
+      const result = await usersCollection.findOne(filter)
+      const checkPin = bcrypt.compareSync(userPin, result.pin);
+      console.log(checkPin);
+      if(!checkPin){
+       return res.send({message:'incorrect pin'})
+      }
+    //  const 
+     if(checkPin&&result.status=='pending'){
+     return  res.send({message:'Request Under Process'})
+      
+    }
+     return  res.send({message:'user'})
+    })
     
     
     // route for register new user
     app.post('/users',async(req,res)=>{
+      //receive user data 
       const userData = req.body;
+      // hash pin 
       const hashedPin=bcrypt.hashSync(userData.pin, salt);
       console.log(hashedPin);
-      const newUserData = {...userData,hashedPin}
+      //make new user data
+      const newUserData = {
+        name: userData?.name,
+        email: userData?.email,
+        phone: userData?.phone,
+        pin: hashedPin,
+        user_balance: userData?.user_balance,
+        agent_balance: userData?.agent_balance,
+        role: userData?.role,
+        status: userData?.status
+      }
       console.log({newUserData});
+
       const email = userData?.email;
       const phone = userData?.phone;
       const filter={email,phone}
+      // find user 
       const userExist = await usersCollection.findOne(filter)
+      // check user is exist 
       if(userExist){
+        // if user exist return
         return res.send('user  already exist')
       }
-      const result =await usersCollection.insertOne(userData)
+      // insert user 
+      const result =await usersCollection.insertOne(newUserData)
+      // send result 
       res.send(result)
       
       
